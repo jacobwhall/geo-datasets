c438b1578db607f878247c9f9edf1d8f7b481d62
Jacob Hall <jwhall@wm.edu>
2023-01-06 16:56:17 -0500

    debug

--- pm25/main.py
+++ pm25/main.py
@@ -298,7 +298,7 @@ def get_config_dict(config_file="config.ini"):
     config.read(config_file)
 
     return {
-        "raw_dir" Path(config["main"]["raw_dir"]),
+        "raw_dir": Path(config["main"]["raw_dir"]),
         "output_dir": Path(config["main"]["output_dir"]),
         "years": [int(y) for y in config["main"]["years"].split(", ")],
         "box_config_path": Path(config["main"]["box_config_path"]),
